9a3259360778e64a80bb9d8d4e21a95e363d442d
Sahilchaudhari-746 <122908368+Sahilchaudhari-746@users.noreply.github.com>
2023-08-25 20:22:09 +0530

    Delete webdevelopement  final directory

--- webdevelopement  final/conn1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "project";
-$username = "root";
-$password = "";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database); ?>
-
